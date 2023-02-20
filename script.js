9142c465539c25e830c6b4f9b36672e695932795
CrocAx <saris3925@gmail.com>
2023-02-20 15:00:38 +0200

    initial commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function() {
 		var columnId = $(this).parent().attr("id");
 		var tasks = JSON.parse(localStorage.getItem(columnId)) || [];
 		for (var i = 0; i < tasks.length; i++) {
-			$(this).append("<li>" + tasks[i] + "</li>");
+			$(this).append("<li><span class='edit'>" + tasks[i] + "</span></li>");
 		}
 	});
 
@@ -17,6 +17,26 @@ $(function() {
 			// Save tasks to Local Storage on list update
 			var columnId = $(this).parent().attr("id");
 			var tasks = $(this).sortable("toArray");
+			localStorage.setItem(columnId, JSON.stringify(tasks));
+		}
+	});
+
+	// Enable task editing
+	$(document).on("dblclick", ".edit", function() {
+		var task = $(this).text();
+		var input = "<input type='text' class='task-edit' value='" + task + "'>";
+		$(this).replaceWith(input);
+		$(".task-edit").focus();
+	});
+
+	// Save edited task on enter key press
+	$(document).on("keypress", ".task-edit", function(event) {
+		if (event.which == 13) {
+			var task = $(this).val();
+			var span = "<span class='edit'>" + task + "</span>";
+			$(this).replaceWith(span);
+			var columnId = $(this).closest(".column").attr("id");
+			var tasks = $("#" + columnId + " .sortable").sortable("toArray");
 			localStorage.setItem(columnId, JSON.stringify(tasks));
 		}
 	});
