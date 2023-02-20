ec76eb71d9410e356304a5f8e98e971715212745
CrocAx <saris3925@gmail.com>
2023-02-20 14:57:17 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 $(function() {
+	// Load saved tasks on page load
+	$(".sortable").each(function() {
+		var columnId = $(this).parent().attr("id");
+		var tasks = JSON.parse(localStorage.getItem(columnId)) || [];
+		for (var i = 0; i < tasks.length; i++) {
+			$(this).append("<li>" + tasks[i] + "</li>");
+		}
+	});
+
+	// Make the lists sortable and draggable
 	$(".sortable").sortable({
 		connectWith: ".sortable",
 		cursor: "move",
 		placeholder: "ui-state-highlight",
+		update: function() {
+			// Save tasks to Local Storage on list update
+			var columnId = $(this).parent().attr("id");
+			var tasks = $(this).sortable("toArray");
+			localStorage.setItem(columnId, JSON.stringify(tasks));
+		}
 	});
 });
